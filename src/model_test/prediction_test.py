b898bb081502158a2195c57251b427fe4b929303
nguu0123 <nguu0123@gmail.com>
2024-08-05 14:14:20 +0300

    add predict test

---
+++ src/model_test/prediction_test.py
@@ -0,0 +1,20 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.applications.resnet50 import (
+    ResNet50,
+    decode_predictions,
+    preprocess_input,
+)
+
+model = ResNet50(weights="imagenet")
+
+img_path = "elephant.jpg"
+img = tf.keras.utils.load_img(img_path, target_size=(224, 224))
+x = tf.keras.utils.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+x = preprocess_input(x)
+
+preds = model.predict(x)
+# decode the results into a list of tuples (class, description, probability)
+# (one such list for each sample in the batch)
+print("Predicted:", decode_predictions(preds, top=3)[0])
