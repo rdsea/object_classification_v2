09152a1b6f362b806b85e351aa7cd936303873d8
nguu0123 <nguu0123@gmail.com>
2024-08-06 10:25:47 +0300

    add load_img

--- src/model_test/tflite_latency_test.py
+++ src/model_test/tflite_latency_test.py
@@ -1,19 +1,12 @@
 import os
-import sys
 import time
 
+import numpy as np
 import tflite_runtime.interpreter as tflite
-
-current_directory = os.path.dirname(os.path.abspath(__file__))
-util_directory = os.path.join(current_directory, "..", "util")
-sys.path.append(util_directory)
-
-from load_image import img_to_array, load_img  # noqa: E402
 
 path = "./tflite_model/"
 dir_list = os.listdir(path)
-
-N = 1
+N = 10
 for dir in dir_list:
     saved_model_path = path + dir
     interpreter = tflite.Interpreter(model_path=saved_model_path)
@@ -25,12 +18,9 @@ for dir in dir_list:
     output_details = interpreter.get_output_details()
 
     input_shape = input_details[0]["shape"]
-    size = input_shape[1]
-    image = load_img("./elephant.jpg", target_size=(size, size))
-    input_data = img_to_array(image, dtype=input_details[0]["dtype"])
     avg_time = 0
-
     for _ in range(N):
+        input_data = np.array(np.random.random_sample(input_shape), dtype=np.float32)
         start_time = time.time()
         interpreter.set_tensor(input_details[0]["index"], input_data)
 
---
+++ src/util/load_image.py
@@ -0,0 +1,129 @@
+"""
+Taken from https://github.com/keras-team/keras/blob/v3.3.3/keras/src/utils/image_utils.py#L187-L293
+"""
+
+import io
+import pathlib
+
+import numpy as np
+from PIL import Image
+from PIL.ImageFile import ImageFile
+
+PIL_INTERPOLATION_METHODS = {
+    "nearest": Image.Resampling.NEAREST,
+    "bilinear": Image.Resampling.BILINEAR,
+    "bicubic": Image.Resampling.BICUBIC,
+    "hamming": Image.Resampling.HAMMING,
+    "box": Image.Resampling.BOX,
+    "lanczos": Image.Resampling.LANCZOS,
+}
+
+
+def load_img(
+    path,
+    color_mode="rgb",
+    target_size=None,
+    interpolation="nearest",
+    keep_aspect_ratio=False,
+):
+    if isinstance(path, io.BytesIO):
+        img = Image.open(path)
+    elif isinstance(path, pathlib.Path | bytes | str):
+        if isinstance(path, pathlib.Path):
+            path = str(path.resolve())
+        with open(path, "rb") as f:
+            img = Image.open(io.BytesIO(f.read()))
+    else:
+        raise TypeError(f"path should be path-like or io.BytesIO, not {type(path)}")
+
+    if color_mode == "grayscale":
+        # if image is not already an 8-bit, 16-bit or 32-bit grayscale image
+        # convert it to an 8-bit grayscale image.
+        if img.mode not in ("L", "I;16", "I"):
+            img = img.convert("L")
+    elif color_mode == "rgba":
+        if img.mode != "RGBA":
+            img = img.convert("RGBA")
+    elif color_mode == "rgb":
+        if img.mode != "RGB":
+            img = img.convert("RGB")
+    else:
+        raise ValueError('color_mode must be "grayscale", "rgb", or "rgba"')
+    if target_size is not None:
+        width_height_tuple = (target_size[1], target_size[0])
+        if img.size != width_height_tuple:
+            if interpolation not in PIL_INTERPOLATION_METHODS:
+                raise ValueError(
+                    "Invalid interpolation method {} specified. Supported "
+                    "methods are {}".format(
+                        interpolation,
+                        ", ".join(PIL_INTERPOLATION_METHODS.keys()),
+                    )
+                )
+            resample = PIL_INTERPOLATION_METHODS[interpolation]
+
+            if keep_aspect_ratio:
+                width, height = img.size
+                target_width, target_height = width_height_tuple
+
+                crop_height = (width * target_height) // target_width
+                crop_width = (height * target_width) // target_height
+
+                # Set back to input height / width
+                # if crop_height / crop_width is not smaller.
+                crop_height = min(height, crop_height)
+                crop_width = min(width, crop_width)
+
+                crop_box_hstart = (height - crop_height) // 2
+                crop_box_wstart = (width - crop_width) // 2
+                crop_box_wend = crop_box_wstart + crop_width
+                crop_box_hend = crop_box_hstart + crop_height
+                crop_box = [
+                    crop_box_wstart,
+                    crop_box_hstart,
+                    crop_box_wend,
+                    crop_box_hend,
+                ]
+                img = img.resize(width_height_tuple, resample, box=crop_box)
+            else:
+                img = img.resize(width_height_tuple, resample)
+    return img
+
+
+def img_to_array(img: ImageFile, dtype=None):
+    """Converts a PIL Image instance to a NumPy array.
+
+    Example:
+
+    ```python
+    from PIL import Image
+    img_data = np.random.random(size=(100, 100, 3))
+    img = keras.utils.array_to_img(img_data)
+    array = keras.utils.image.img_to_array(img)
+    ```
+
+    Args:
+        img: Input PIL Image instance.
+        dtype: Dtype to use. `None` means the global setting
+            `keras.backend.floatx()` is used (unless you changed it, it
+            defaults to `"float32"`).
+
+    Returns:
+        A 3D NumPy array.
+    """
+
+    if dtype is None:
+        dtype = np.float32
+    # NumPy array x has format (height, width, channel)
+    # or (channel, height, width)
+    # but original PIL image has format (width, height, channel)
+    x = np.asarray(img, dtype=dtype)
+    if len(x.shape) == 3:
+        pass
+    elif len(x.shape) == 2:
+        x = x.reshape((x.shape[0], x.shape[1], 1))
+    else:
+        raise ValueError(f"Unsupported image shape: {x.shape}")
+
+    x = np.expand_dims(x, axis=0)
+    return x
