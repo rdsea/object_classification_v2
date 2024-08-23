0dc37d043aaef002fcd34238e03bff0d453425d2
nguu0123 <nguu0123@gmail.com>
2024-08-23 16:39:17 +0300

    update Dockerfile and add build, run script

--- src/inference/docker_build.sh
+++ src/inference/docker_build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # NOTE: Need to be run in the parent directory of the 'inference' directory
+# If qemu is not available, host system may not be able to build the docker image for arm64 platform
 
 if [ "$1" == "cpu" ] || [ "$1" == "cuda" ]; then
   TAG=$1
