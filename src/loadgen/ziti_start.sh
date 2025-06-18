e7367a1499f7b02b18e7fa6858b7959adccac217
hong3nguyen <tringuyennht@gmail.com>
2025-06-18 18:40:07 +0300

    edit the ziti writing to logs

--- src/loadgen/ziti_start.sh
+++ src/loadgen/ziti_start.sh
@@ -3,7 +3,7 @@
 sed 's/^::1/#&/' /etc/hosts >/tmp/hosts && cat /tmp/hosts >/etc/hosts
 
 # ziti-edge-tunnel takeing sceret from opt
-ziti-edge-tunnel run -i /opt/secret.json >/dev/null 2>&1 &
+ziti-edge-tunnel run -i /opt/secret.json >/var/log/ziti-edge-tunnel.log 2>&1 &
 
 # main application
 ./entrypoint.sh
