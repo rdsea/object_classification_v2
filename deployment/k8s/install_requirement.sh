4402c0ea846ee0cbba7725dd790943ed6b334603
nguu0123 <nguu0123@gmail.com>
2025-06-24 10:58:47 +0300

    Simplify HPA configs and enhance Hubble settings

--- deployment/k8s/install_requirement.sh
+++ deployment/k8s/install_requirement.sh
@@ -20,6 +20,9 @@ cilium install --version 1.17.4 \
   --set prometheus.enabled=true \
   --set operator.prometheus.enabled=true \
   --set hubble.enabled=true \
+  --set hubble.relay.enabled=true \
+  --set hubble.ui.enabled=true \
+  --set hubble.ui.baseUrl="/hubble/" \
   --set hubble.metrics.enableOpenMetrics=true \
   --set hubble.metrics.enabled="{dns,drop,tcp,flow,port-distribution,icmp,httpV2:exemplars=true;labelsContext=source_ip\,source_namespace\,source_workload\,destination_ip\,destination_namespace\,destination_workload\,traffic_direction}"
 
