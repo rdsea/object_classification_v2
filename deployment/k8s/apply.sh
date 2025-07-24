1bd6ee2827baa3a86536520e7b19d49f25c2f028
nguu0123 <nguu0123@gmail.com>
2025-07-24 17:03:02 +0300

    Increase replica count and centralize HPA configs
    - Update replica count to 2 for all deployments to improve availability.
    - Remove individual HPA definitions and consolidate them into a single `hpa.yaml` for cleaner management.
    - Modify `apply.sh` and `remove.sh` scripts to handle the new centralized HPA file with an optional flag.

--- deployment/k8s/apply.sh
+++ deployment/k8s/apply.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
+
+apply_hpa=false
+for arg in "$@"; do
+  if [ "$arg" == "--hpa" ]; then
+    apply_hpa=true
+  fi
+done
+
 kubectl apply -f EfficientNetB0.yaml
 kubectl apply -f MobileNetV2.yaml
 kubectl apply -f ensemble.yaml
 kubectl apply -f preprocessing.yaml
+
+if [ "$apply_hpa" = true ]; then
+  kubectl apply -f hpa.yaml
+fi
--- deployment/k8s/remove.sh
+++ deployment/k8s/remove.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
+
+apply_hpa=false
+for arg in "$@"; do
+  if [ "$arg" == "--hpa" ]; then
+    apply_hpa=true
+  fi
+done
 kubectl delete -f EfficientNetB0.yaml
 kubectl delete -f MobileNetV2.yaml
 kubectl delete -f ensemble.yaml
 kubectl delete -f preprocessing.yaml
+
+if [ "$apply_hpa" = true ]; then
+  kubectl delete -f hpa.yaml
+fi
