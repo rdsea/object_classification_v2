6a05ee6c302d5b0e54343cfe81616ab319db583a
hong3nguyen <tringuyennht@gmail.com>
2025-09-22 17:13:05 +0300

    enable the change for ensemble url

--- src/preprocessing/preprocessing.py
+++ src/preprocessing/preprocessing.py
@@ -48,6 +48,9 @@ if os.environ.get("OPENZITI"):
     ENSEMBLE_SERVICE_URL = "http://ensemble.miniziti.private:5011/ensemble_service"
 
 
+if os.environ.get("ENSEMBLE_ENDPOINT"):
+    ENSEMBLE_SERVICE_URL = os.environ.get("ENSEMBLE_ENDPOINT")
+
 current_directory = os.path.dirname(os.path.abspath(__file__))
 util_directory = os.path.join(current_directory, "..", "util")
 sys.path.append(util_directory)
